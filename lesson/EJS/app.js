bc78826076c27cc9fca81765f94007179b5103bb
Chieh <franklu00313@gmail.com>
2022-04-28 21:44:36 +0800

    EJS Completed!

--- lesson/EJS/app.js
+++ lesson/EJS/app.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // for app.post()
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+// form
+app.post("/form", (req, res) => {
+  // let { name, age } = req.body;
+  console.log(req.body);
+  res.render("form.ejs", req.body);
+});
+
+// array of object (simulate database)
+app.get("/array", (req, res) => {
+  const languages = [
+    { name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+    { name: "Java", rating: 8.6, popularity: 8.2, trending: "same" },
+    { name: "C++", rating: 6.6, popularity: 7.7, trending: "same" },
+    { name: "PHP", rating: 2.5, popularity: 4.7, trending: "decreasing" },
+    { name: "JavaScript", rating: 8.5, popularity: 8.1, trending: "same" },
+  ];
+  res.render("array.ejs", { languages }); // 實際是pass {languages:languages} 這個裝array的obj出去
+});
+
+// pattern => 要放最後面，不然會把/array的路徑蓋過去
 app.get("/:name", (req, res) => {
   let { name } = req.params;
-  res.render("index.ejs", { name });
+  res.render("name.ejs", { name });
 });
 
 app.listen(3000, () => {
---
+++ try/a.js
@@ -0,0 +1,11 @@
+const languages = [
+  { name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+  { name: "Java", rating: 8.6, popularity: 8.2, trending: "same" },
+  { name: "C++", rating: 6.6, popularity: 7.7, trending: "same" },
+  { name: "PHP", rating: 2.5, popularity: 4.7, trending: "decreasing" },
+  { name: "JavaScript", rating: 8.5, popularity: 8.1, trending: "same" },
+];
+
+languages.forEach((i) => {
+  console.log(i);
+});
